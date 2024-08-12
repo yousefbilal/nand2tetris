8857fbf5d319a77e4c8a1b5c5e4827e9e74b578a
Yousef Irshaid <yousefbilal17@gmail.com>
2024-08-12 19:12:26 +0400

    completed the label-less assembler

--- projects/6/code.go
+++ projects/6/code.go
@@ -3,25 +3,25 @@ package main
 import "log"
 
 var destMap = map[string]string{
-	"null": "000",
-	"M":    "001",
-	"D":    "010",
-	"MD":   "011",
-	"A":    "100",
-	"AM":   "101",
-	"AD":   "110",
-	"AMD":  "111",
+	"":    "000",
+	"M":   "001",
+	"D":   "010",
+	"MD":  "011",
+	"A":   "100",
+	"AM":  "101",
+	"AD":  "110",
+	"AMD": "111",
 }
 
 var jumpMap = map[string]string{
-	"null": "000",
-	"JGT":  "001",
-	"JEQ":  "010",
-	"JGE":  "011",
-	"JLT":  "100",
-	"JNE":  "101",
-	"JLE":  "110",
-	"JMP":  "111",
+	"":    "000",
+	"JGT": "001",
+	"JEQ": "010",
+	"JGE": "011",
+	"JLT": "100",
+	"JNE": "101",
+	"JLE": "110",
+	"JMP": "111",
 }
 
 var compMap = map[string]string{
--- projects/6/main.go
+++ projects/6/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+)
+
+const (
+	assemblyFileExtension = ".asm"
+	hackFileExtension     = ".hack"
 )
 
 func usage() string {
 
 	return fmt.Sprintf("Usage: %s <filename>", filepath.Base(os.Args[0]))
+}
+
+func replaceExtension(fileName string, newExt string) string {
+	return fileName[:len(fileName)-len(filepath.Ext(fileName))] + newExt
 }
 func main() {
 	if len(os.Args) < 2 {
@@ -16,6 +27,35 @@ func main() {
 		return
 	}
 
-	filename := os.Args[1]
-	fmt.Println(filename)
+	fileName := os.Args[1]
+	if filepath.Ext(fileName) != assemblyFileExtension {
+		log.Fatalf("File name must end with %v\n", assemblyFileExtension)
+	}
+	inputFile, err := os.Open(fileName)
+	if err != nil {
+		log.Fatalln("Cannot open file: ", err)
+	}
+	defer inputFile.Close()
+
+	outputFile, err := os.Create(replaceExtension(fileName, hackFileExtension))
+	if err != nil {
+		log.Fatalln("Cannot create file: ", err)
+	}
+
+	parser := NewParser(inputFile)
+
+	for parser.HasMoreCommands() {
+		parser.Advance()
+		switch parser.CommandType() {
+		case A_COMMAND, L_COMMAND:
+			symbol, _ := strconv.ParseUint(parser.Symbol(), 10, 16)
+			fmt.Fprintf(outputFile, "%016b\n", symbol)
+		case C_COMMAND:
+			dest := Dest(parser.Dest())
+			comp := Comp(parser.Comp())
+			jump := Jump(parser.Jump())
+			fmt.Fprintf(outputFile, "111%v%v%v\n", comp, dest, jump)
+		}
+	}
+
 }
